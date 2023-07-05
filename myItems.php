d5ab9c0e49d150225c4b2456b75c5ad61ab1af23
JulienFX <jujustor@gmail.com>
2023-07-05 16:52:17 +0100

    delete avec pb db retard

--- myItems.php
+++ myItems.php
@@ -14,8 +14,8 @@
       </nav>
       <div class="content">
       <?php
-        require_once('connexionDB.php');
-        global $conn;
+      require_once('connexionDB.php');
+      global $conn;
         // Fetch items from the table
         $username = $_SESSION["username"];
         $sql = "SELECT i.id,nameItem,descriptions,price,categories,available,sellType,idLink,link FROM items as i natural join have as h inner join picturesvideos as pv on h.idLink=pv.id where i.id in (SELECT idItem from sell where username ='$username')  group by i.id";
@@ -30,11 +30,20 @@
                 echo '<img src='.$row["link"].' alt="">';
                 echo '<p>' . $row["descriptions"] . '</p>';
                 echo '<p>£' . $row["price"] . '</p>';
-                echo '<button>edit item</button>';
-                echo '<button>delete item</button>';
+                echo'<a href="myItems.php?editId='.$row['id'].'" >edit item </a>';
+                echo'<a href="myItems.php?deleteId='.$row['id'].'" >delete item</a>';
                 echo '</div>';
                 echo '</div>';
             }
+        }
+        
+        if(isset($_GET["editId"])){
+
+        }else if(isset($_GET["deleteId"])){
+          $id = $_GET["deleteId"];
+          $delete = mysqli_query($conn,"DELETE FROM items where id='$id'");
+          header("location:myItems.php");
+          die();
         }
     ?>
         </div>
