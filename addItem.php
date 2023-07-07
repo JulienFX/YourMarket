9305a1048bff3c1512b33ebe25d6ee537b5afb1b
JulienFX <jujustor@gmail.com>
2023-07-07 14:12:46 +0100

    maj form ajout item

--- addItem.php
+++ addItem.php
@@ -3,6 +3,24 @@
 <html>
 <head>
   <link rel="stylesheet" type="text/css" href="Constant/styles.css">
+  <script>
+        function afficherChampTexte() {
+            var choix = document.getElementById("choix").value;
+            var champPrixVente = document.getElementById("champ-prix-vente");
+            var champPrixDepart = document.getElementById("champ-prix-depart");
+            
+            if (choix === "1") {
+                champPrixVente.style.display = "block";
+                champPrixDepart.style.display = "none";
+            } else if (choix === "2") {
+                champPrixVente.style.display = "none";
+                champPrixDepart.style.display = "block";
+            } else {
+                champPrixVente.style.display = "none";
+                champPrixDepart.style.display = "none";
+            }
+        }
+    </script>
 </head>
 <body>
     <?php
@@ -86,12 +104,20 @@
                 <option value="2" id="electronic">Electronics</option>
             </select><br>
             <label for="sellType">Sell type : </label>
-            <select name="sellType" id="category" required>
-                <option value="1" id="cake">Bids</option>
-                <option value="2" id="electronic">Orders</option>
-                <option value="3" id="electronic">Offers</option>
-                <!-- ADD OTHERS VALUES -->
-            </select><br>
+            <select id="choix" onchange="afficherChampTexte()">
+                <option value="">Select an option</option>
+                <option value="1">Bids & Offer</option>
+                <option value="2">Auctions</option>
+            </select>
+            <br><br>
+            <div id="champ-prix-vente" style="display: none;">
+                <label for="prix-vente">Prix de vente :</label>
+                <input type="text" id="prix-vente">
+            </div>
+            <div id="champ-prix-depart" style="display: none;">
+                <label for="prix-depart">Prix de départ :</label>
+                <input type="text" id="prix-depart">
+            </div>
 
             <label for="photos">Photos:</label>
             <input type="file" name="photos[]" id="photos" multiple required><br>
