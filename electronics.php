7ae98e439709c134517c351e58f63a83616f3857
JulienFX <jujustor@gmail.com>
2023-07-11 18:12:43 +0100

    bids

--- electronics.php
+++ electronics.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <title>Available Items</title>
     <style>
@@ -31,13 +30,12 @@
     </style>
     <link rel="stylesheet" type="text/css" href="Constant/styles.css">
 </head>
-
 <body>
-
     <head>
         <?php
         session_start();
-        include('Constant/head.php'); ?>
+        include('Constant/head.php');
+        ?>
     </head>
     <div class="page">
         <nav>
@@ -68,7 +66,6 @@
                             echo '<button class="add-to-cart">Add to Cart</button><br>';
                             echo '<button onclick="openPayment();">Buy Now</button><br>';
                             echo '<button >Negociate the price</button>';
-                            
                         }else{
                             if ($currentDatetime >= $row["endDate"]) {
                                 // Le timer est expiré, effectuer les actions nécessaires
@@ -82,23 +79,98 @@
                                 echo "Remaining time : " . $remainingTime;
                             }
                             echo '<p>Starting price for bids : £' . $row["price"] . '</p>';
-                            echo '<button>Auction now </button><br>';
+                            echo '<a href="javascript:void(0);" onclick="confirmBid('.$row['id'].', '.$row['price'].');">auction now</a> <br>';
+                            // echo '<input type="text" id="bidInput_'.$row["id"].'" name="'.$row["id"].'">';
                         }
                         echo '</div>';
                         echo '</div>';
                     }
                 }
                 // Close the database connection
+                // if(isset($_GET["itemId"]) && isset($_GET["bidValue"])){
+                //     $id = $_GET["itemId"];
+                //     $price = $_POST["bidValue"];
+                //     $sql = "SELECT count(*) from bids where id='$id'";
+                //     $conn->query($sql);
+                //     $row = $result->fetch_assoc();
+                //     $count = $row['count'];
+                //     if($count==0){
+                //         $sql = "INSERT INTO bids (itemId VALUES ($id,"
+                //     }
+                // }
+                if (isset($_GET['itemId']) && isset($_GET['bidValue'])) {
+                    $itemId = $_GET['itemId'];
+                    $bidValue = $_GET['bidValue'];
+                
+                    $sql = "SELECT * FROM bids WHERE itemId = '$itemId'";
+                    $result = $conn->query($sql);
+                
+                    if ($result->num_rows > 0) {
+                        // Une ligne existe déjà, effectuer une mise à jour
+                        $sql = "UPDATE bids SET price = '$bidValue' WHERE itemId = '$itemId'";
+                
+                        if ($conn->query($sql) === TRUE) {
+                            echo "Mise à jour réussie";
+                        } else {
+                            echo "Erreur lors de la mise à jour : " . $conn->error;
+                        }
+                    } else {
+                        // Aucune ligne existante, effectuer une insertion
+                        // Vérifier si le prix est de type entier
+                        if (is_numeric($bidValue)) {
+                            $username = $_SESSION["username"];
+                
+                            // Vérifier si l'utilisateur connecté a déjà fait une enchère pour l'ID spécifié
+                            $sql = "SELECT * FROM place WHERE bidId IN (SELECT id FROM bids WHERE itemId = '$itemId') AND username = '$username'";
+                            $result = $conn->query($sql);
+                
+                            if ($result->num_rows > 0) {
+                                echo "Vous avez déjà fait une enchère pour cet article";
+                            } else {
+                                // Insérer une nouvelle ligne dans la table bids
+                                $sql = "INSERT INTO bids (itemId, price) VALUES ('$itemId', '$bidValue')";
+                
+                                if ($conn->query($sql) === TRUE) {
+                                    // Récupérer le bidId de la nouvelle ligne insérée dans la table bids
+                                    $bidId = $conn->insert_id;
+                
+                                    // Insérer une nouvelle ligne dans la table place
+                                    $sql = "INSERT INTO place (bidId, username) VALUES ('$bidId', '$username')";
+                
+                                    if ($conn->query($sql) === TRUE) {
+                                        echo "Enchère et emplacement insérés avec succès";
+                                    } else {
+                                        echo "Erreur lors de l'insertion dans la table place : " . $conn->error;
+                                    }
+                                } else {
+                                    echo "Erreur lors de l'insertion dans la table bids : " . $conn->error;
+                                }
+                            }
+                        } else {
+                            echo "Le prix doit être un nombre entier";
+                        }
+                    }
+                } else {
+                    // echo "Les variables itemId et bidValue doivent être spécifiées dans l'URL";
+                }
                 $conn->close();
                 ?>
             </div>
         </div>
     </div>
     <script>
-    function openPayment() {
-      window.open("payment.php", "_blank");
-    }
-  </script>
-</body>
+        function openPayment() {
+            window.open("payment.php", "_blank");
+        }
 
+        function confirmBid(itemId, startingPrice) {
+            var bidValue = prompt("Enter your bid price:");
+
+            if (bidValue !== null) {
+                var url = "electronics.php?itemId=" + itemId + "&bidValue=" + encodeURIComponent(bidValue);
+                window.location.href = url;
+            }
+        }
+    </script>
+</body>
 </html>
